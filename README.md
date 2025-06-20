17275af8454c21e53d998632cb8f16205166c95a
Noor Piash <noorpiash1234@gmail.com>
2025-06-21 04:58:42 +0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ema-john-client
+
+Updated on: June 20, 2025
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,26 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    const { count } = useLoaderData();
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage)
+
+
+
+    const pages = [...Array(numberOfPages).keys()];
+
+
+    // const pages = []
+
+    // for (let i = 0; i < numberOfPages; i++) {
+    //     pages.push(i);
+    // }
+    // console.log(pages)
 
     useEffect(() => {
         fetch('http://localhost:5000/products')
@@ -82,6 +97,12 @@ const Shop = () => {
                     </Link>
                 </Cart>
             </div>
+            <div className='pagination'>
+
+                {
+                    pages.map(page => <button key={page}>{page}</button>)
+
+                }</div>
         </div>
     );
 };
